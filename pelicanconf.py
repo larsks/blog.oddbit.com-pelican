80fcb94b42f8624f102b19e853fa3832fac06839
Lars Kellogg-Stedman <lars@redhat.com>
2018-01-26 10:21:37 -0500

    initial import

---
+++ pelicanconf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Lars Kellogg-Stedman'
+SITENAME = 'oddbit.com'
+SITEURL = ''
+SITESUBTITLE = 'One of these things is not like the others'
+
+PATH = 'content'
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
+
+TIMEZONE = 'US/Eastern'
+
+DEFAULT_CATEGORY = 'Blog'
+DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 5
+RELATIVE_URLS = True
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+STATIC_PATHS = ['images', 'assets']
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = [
+    'md-metayaml',
+    'summary'
+]
+
+MENUITEMS = (
+    ('Archive', '/archives.html'),
+)
+
+SOCIAL = (
+    ('larsks@twitter', 'http://twitter.com/larsks'),
+    ('larsks@github', 'http://github.com/larsks'),
+    ('larsks@freenode', 'https://webchat.freenode.net/'),
+)
+
+DISPLAY_CATEGORIES_ON_MENU = False
+
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAG_URL = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+CATEGORY_SAVE_AS = 'category/{slug}.html'
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+DAY_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/index.html'
+AUTHOR_SAVE_AS = ''
+
+SUMMARY_END_MARKER = '<!-- more -->'
+SUMMARY_BEGIN_MARKER = '<!-- begin -->'
+
+THEME = 'themes/tuxlite_tbs'
+GITHUB_URL = 'https://github.com/larsks/blog.oddbit.com-posts'
+PROJECT_ROOT = '/home/lars/tmp/blog/content'
---
+++ publishconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = 'http://blog.oddbit.com'
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = 'atom.xml'
+FEED_ALL_RSS = 'rss.xml'
+CATEGORY_FEED_ATOM = None
+TAG_FEED_RSS = 'tag/%s/rss/index.html'
+TAG_FEED_ATOM = 'tag/%s/atom/index.html'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+
+DISQUS_SITENAME = "oddbit"
+GOOGLE_ANALYTICS = "UA-45791426-1"
